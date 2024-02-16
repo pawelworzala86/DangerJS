37785c750903ae1c59e21013eedd54b91a0f6543
pawelworzala86 <pawelworzala86@gmail.com>
2024-02-16 17:59:18 +0100

    fs

--- source/object6fs.js
+++ source/object6fs.js
@@ -1,30 +1,5 @@
 
-class File{
-    constructor(){
-        this.handle = 0
-        this.fsize = 0
-        this.buffor = 0
-    }
-    readRile(fileName){
-        this.handle = CreateFile(fileName, GENERIC_READ,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL, 0)
-        this.fsize = GetFileSize(this.handle, 0)
-        this.buffor = alloc(this.fsize)
-        ReadFile(this.handle, &this.buffor, this.fsize, 0, 0)
-        CloseHandle(this.handle)
-    }
-    open(fileName){
-        this.handle = CreateFile(fileName, GENERIC_READ,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL, 0)
-        this.fsize = GetFileSize(this.handle, 0)
-        this.buffor = alloc(this.fsize)
-    }
-    read(fsize){
-        ReadFile(this.handle, &this.buffor, fsize, 0, 0)
-    }
-    close(){
-        CloseHandle(this.handle)
-    }
-}
-var file = new File()
+import 'fs.js'
 
 
 
