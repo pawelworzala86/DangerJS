93632a571fb4a98ab9dbeda91e014a22dcfffd49
pawelworzala86 <pawelworzala86@gmail.com>
2024-02-27 15:25:06 +0100

    auto save

--- source/arrayObj.js
+++ source/arrayObj.js
@@ -2,9 +2,14 @@
 class TestClass{
     constructor(){
         this.valA = 2344
+        this.array = 0
         this.valB = 666
     }
     print(){
+        mov this.array, alloc(128)
+        mov this.array[0], 11
+        mov this.array[1], 11
+
         mov rax,this.valB
         printf('valB %i', rax)
     }
